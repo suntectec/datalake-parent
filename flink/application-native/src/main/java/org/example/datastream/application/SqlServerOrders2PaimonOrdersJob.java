1d64b3072d7daa4ff38df13b1f269e6dd9f21cac
suntectec <suntectec@163.com>
2025-08-04 18:01:20 +0800

    [WIP] Update CDC Ingestion

--- flink/application-native/src/main/java/org/example/datastream/application/SqlServerOrders2PaimonOrdersJob.java
+++ flink/application-native/src/main/java/org/example/datastream/application/SqlServerOrders2PaimonOrdersJob.java
@@ -1,20 +1,14 @@
 package org.example.datastream.application;
 
-import org.apache.flink.streaming.api.datastream.DataStream;
+import org.apache.flink.api.common.eventtime.WatermarkStrategy;
+import org.apache.flink.cdc.connectors.base.options.StartupOptions;
+import org.apache.flink.cdc.connectors.sqlserver.source.SqlServerSourceBuilder;
+import org.apache.flink.cdc.connectors.sqlserver.source.SqlServerSourceBuilder.SqlServerIncrementalSource;
+import org.apache.flink.cdc.debezium.JsonDebeziumDeserializationSchema;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
-import org.apache.paimon.catalog.CatalogLoader;
-import org.apache.paimon.catalog.Identifier;
-import org.apache.paimon.flink.FlinkCatalogFactory;
-import org.apache.paimon.flink.sink.cdc.RichCdcRecord;
-import org.apache.paimon.flink.sink.cdc.RichCdcSinkBuilder;
-import org.apache.paimon.options.Options;
-import org.apache.paimon.table.Table;
-import org.apache.paimon.types.DataTypes;
 import org.example.util.MyParameter;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
-import static org.apache.paimon.types.RowKind.INSERT;
 
 /**
  * Exception in thread "main" java.lang.UnsatisfiedLinkError: 'boolean org.apache.hadoop.io.nativeio.NativeIO$Windows.access0(java.lang.String, int)'
@@ -28,59 +22,89 @@ public class SqlServerOrders2PaimonOrdersJob {
     // Define Logger at the class level
     private static final Logger logger = LoggerFactory.getLogger(SqlServerOrders2PaimonOrdersJob.class);
 
-    public static void run(String warehouse, String s3_endpoint, String s3_access_key, String s3_secret_key) throws Exception {
+    public static void run(String sqlserver_host, String sqlserver_port, String sqlserver_username, String sqlserver_password,
+                           String warehouse, String s3_endpoint, String s3_access_key, String s3_secret_key) throws Exception {
+
+        SqlServerIncrementalSource<String> sqlServerSource =
+                new SqlServerSourceBuilder<String>()
+                        .hostname(sqlserver_host)
+                        .port(Integer.parseInt(sqlserver_port))
+                        .databaseList("inventory")
+                        .tableList("INV.orders")
+                        .username(sqlserver_username)
+                        .password(sqlserver_password)
+                        .deserializer(new JsonDebeziumDeserializationSchema())
+                        .startupOptions(StartupOptions.initial())
+                        .build();
+
         StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
-        // for CONTINUOUS_UNBOUNDED source, set checkpoint interval
-        // env.enableCheckpointing(60_000);
+        // enable checkpoint for CONTINUOUS_UNBOUNDED source, set checkpoint interval
+        env.enableCheckpointing(3000);
 
-        DataStream<RichCdcRecord> dataStream =
-                env.fromElements(
-                        RichCdcRecord.builder(INSERT)
-                                .field("order_id", DataTypes.BIGINT(), "123")
-                                .field("price", DataTypes.DOUBLE(), "62.2")
-                                .build(),
-                        // dt field will be added with schema evolution
-                        RichCdcRecord.builder(INSERT)
-                                .field("order_id", DataTypes.BIGINT(), "245")
-                                .field("price", DataTypes.DOUBLE(), "82.1")
-                                .field("dt", DataTypes.TIMESTAMP(), "2023-06-12 20:21:12")
-                                .build());
+        // set the source parallelism to 2
+        env.fromSource(
+                        sqlServerSource,
+                        WatermarkStrategy.noWatermarks(),
+                        "SqlServerIncrementalSource")
+                .setParallelism(2)
+                .print()
+                .setParallelism(1);
 
-        Identifier identifier = Identifier.create("my_db", "T");
-        Options catalogOptions = new Options();
-        catalogOptions.set("warehouse", warehouse);
-        catalogOptions.set("s3.endpoint", s3_endpoint);
-        catalogOptions.set("s3.access-key", s3_access_key);
-        catalogOptions.set("s3.secret-key", s3_secret_key);
-        catalogOptions.set("s3.path.style.access", "true");
-        CatalogLoader catalogLoader =
-                () -> FlinkCatalogFactory.createPaimonCatalog(catalogOptions);
-        catalogLoader.load().createDatabase("my_db", true);
-        catalogLoader.load().createTable(identifier,
-                org.apache.paimon.schema.Schema.newBuilder()
-                        .column("order_id", DataTypes.BIGINT())
-                        .column("price", DataTypes.DOUBLE())
-                        .column("dt", DataTypes.TIMESTAMP())
-                        .primaryKey("order_id")
-                        .build(), true);
+        // DataStream<RichCdcRecord> dataStream =
+        //         env.fromElements(
+        //                 RichCdcRecord.builder(INSERT)
+        //                         .field("order_id", DataTypes.BIGINT(), "123")
+        //                         .field("price", DataTypes.DOUBLE(), "62.2")
+        //                         .build(),
+        //                 // dt field will be added with schema evolution
+        //                 RichCdcRecord.builder(INSERT)
+        //                         .field("order_id", DataTypes.BIGINT(), "245")
+        //                         .field("price", DataTypes.DOUBLE(), "82.1")
+        //                         .field("dt", DataTypes.TIMESTAMP(), "2023-06-12 20:21:12")
+        //                         .build());
 
-        Table table = catalogLoader.load().getTable(identifier);
 
-        new RichCdcSinkBuilder(table)
-                .forRichCdcRecord(dataStream)
-                .identifier(identifier)
-                .catalogLoader(catalogLoader)
-                .build();
+        // Identifier identifier = Identifier.create("my_db", "T");
+        // Options catalogOptions = new Options();
+        // catalogOptions.set("warehouse", warehouse);
+        // catalogOptions.set("s3.endpoint", s3_endpoint);
+        // catalogOptions.set("s3.access-key", s3_access_key);
+        // catalogOptions.set("s3.secret-key", s3_secret_key);
+        // catalogOptions.set("s3.path.style.access", "true");
+        // CatalogLoader catalogLoader =
+        //         () -> FlinkCatalogFactory.createPaimonCatalog(catalogOptions);
+        // catalogLoader.load().createDatabase("my_db", true);
+        // catalogLoader.load().createTable(identifier,
+        //         org.apache.paimon.schema.Schema.newBuilder()
+        //                 .column("order_id", DataTypes.BIGINT())
+        //                 .column("price", DataTypes.DOUBLE())
+        //                 .column("dt", DataTypes.TIMESTAMP())
+        //                 .primaryKey("order_id")
+        //                 .build(), true);
+        //
+        // Table table = catalogLoader.load().getTable(identifier);
+        //
+        // new RichCdcSinkBuilder(table)
+        //         .forRichCdcRecord(sqlServerIncrementalSource)
+        //         .identifier(identifier)
+        //         .catalogLoader(catalogLoader)
+        //         .build();
 
-        env.execute();
+        env.execute("Print SqlServer Snapshot + Change Stream");
     }
 
     public static void main(String[] args) throws Exception {
+        String sqlserver_host = MyParameter.getParameter("dev", "sqlserver.host");
+        String sqlserver_port = MyParameter.getParameter("dev", "sqlserver.port");
+        String sqlserver_username = MyParameter.getParameter("dev", "sqlserver.username");
+        String sqlserver_password = MyParameter.getParameter("dev", "sqlserver.password");
+
         String warehouse = MyParameter.getParameter("dev", "warehouse");
         String s3_endpoint = MyParameter.getParameter("dev", "s3.endpoint");
         String s3_access_key = MyParameter.getParameter("dev", "s3.access-key");
         String s3_secret_key = MyParameter.getParameter("dev", "s3.secret-key");
 
-        SqlServerOrders2PaimonOrdersJob.run(warehouse, s3_endpoint, s3_access_key, s3_secret_key);
+        SqlServerOrders2PaimonOrdersJob.run(sqlserver_host, sqlserver_port, sqlserver_username, sqlserver_password,
+                warehouse, s3_endpoint, s3_access_key, s3_secret_key);
     }
 }
--- flink/application-native/src/main/java/org/example/datastream/development/SQLCreateIntegrationTableAPIJob.java
+++ flink/application-native/src/main/java/org/example/datastream/development/SQLCreateIntegrationTableAPIJob.java
@@ -3,7 +3,7 @@ package org.example.datastream.development;
 import org.apache.flink.connector.datagen.table.DataGenConnectorOptions;
 import org.apache.flink.table.api.*;
 
-public class MixTableAPIandSQLJob {
+public class SQLCreateIntegrationTableAPIJob {
     public static void main(String[] args) {
         // StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
         // StreamTableEnvironment tEnv = StreamTableEnvironment.create(env);
@@ -46,5 +46,14 @@ public class MixTableAPIandSQLJob {
 
         // emit the result Table to the registered TableSink
         pipeline.execute();
+
+        // Emit a Table API result Table to a TableSink, same for SQL result
+        // TableResult tableResult = table1.insertInto("SinkTable").execute();
+
+        // tableEnv.executeSql("SELECT * FROM SourceTable").print();
+        // tableEnv.executeSql("SELECT * FROM SinkTable").print();
+        // tableResult.print();
+        // Table sourceTable = tableEnv.from("SourceTable").select($("f0"));
+        // tableEnv.createTemporaryView("sourceTable", sourceTable);
     }
 }
